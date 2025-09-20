0e002bf3e0d07c74728b3ef6fce273bcd81259c1
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-09-20 15:44:07 +0100

    Create FAQ page

---
+++ src/pages/FAQ.tsx
@@ -0,0 +1,145 @@
+import Navbar from "../components/Navigation";
+import Footer from "../components/Footer";
+
+export default function FAQ() {
+  return (
+    <>
+      <Navbar />
+
+      <section className="block block--white block--top-castrated">
+        <h2>Frequently Asked Questions</h2>
+      </section>
+
+      <section className="block block--white block-beaneath-title">
+        <div className="container--large">
+          <div className="container--grey">
+            <p className="faq">
+              <span className="faq__qa">Question:</span> What are the fees?
+            </p>
+          </div>
+          <div className="container--white">
+            <p className="faq">
+              <span className="faq__qa">Answer:</span> Lorem ipsum dolor sit,
+              amet consectetur adipisicing elit. Ducimus voluptates corporis
+              delectus illum sed architecto facere repudiandae libero vel
+              exercitationem!
+            </p>
+          </div>
+        </div>
+
+        <div className="divider"></div>
+
+        <div className="container--large">
+          <div className="container--grey">
+            <p className="faq">
+              <span className="faq__qa">Question:</span> Do you offer a sliding
+              scale for people with low income?
+            </p>
+          </div>
+          <div className="container--white">
+            <p className="faq">
+              <span className="faq__qa">Answer:</span> Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Inventore modi quam animi eos
+              unde atque dignissimos, minima amet. Inventore ipsa aspernatur
+              porro labore dolorum provident explicabo illo magni? Quo, vitae.
+            </p>
+          </div>
+        </div>
+
+        <div className="divider"></div>
+
+        <div className="container--large">
+          <div className="container--grey">
+            <p className="faq">
+              <span className="faq__qa">Question:</span> Haghamentarabashkul?
+            </p>
+          </div>
+          <div className="container--white">
+            <p className="faq">
+              <span className="faq__qa">Answer:</span> Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Minus iure quibusdam in
+              voluptate quasi sint perspiciatis saepe quisquam.
+            </p>
+          </div>
+        </div>
+
+        <div className="divider"></div>
+
+        <div className="container--large">
+          <div className="container--grey">
+            <p className="faq">
+              <span className="faq__qa">Question:</span> Parampashkul?
+            </p>
+          </div>
+          <div className="container--white">
+            <p className="faq">
+              <span className="faq__qa">Answer:</span> Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Iusto, dolor!
+            </p>
+          </div>
+        </div>
+
+        <div className="divider"></div>
+
+        <div className="container--large">
+          <div className="container--grey">
+            <p className="faq">
+              <span className="faq__qa">Question:</span> Crocolisk?
+            </p>
+          </div>
+          <div className="container--white">
+            <p className="faq">
+              <span className="faq__qa">Answer:</span> Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Vero eaque tempora, ut labore
+              iste, quidem nostrum possimus dignissimos ab, accusamus officia
+              corporis! Vero, dolore voluptatum nulla tempora eligendi quos
+              nobis, est laboriosam veritatis dolor similique quibusdam aperiam
+              distinctio iste dicta.
+            </p>
+          </div>
+        </div>
+
+        <div className="divider"></div>
+
+        <div className="container--large">
+          <div className="container--grey">
+            <p className="faq">
+              <span className="faq__qa">Question:</span> Zhu Jun?
+            </p>
+          </div>
+          <div className="container--white">
+            <p className="faq">
+              <span className="faq__qa">Answer:</span> Lorem ipsum dolor sit,
+              amet consectetur adipisicing elit. Culpa iure asperiores minima
+              hic dolores necessitatibus illo unde ad quam maiores est incidunt
+              laboriosam esse consequuntur ipsa vero totam, veniam dolore
+              deleniti. Quam quisquam molestiae expedita? Pariatur magni quae
+              eos atque eaque, vero a, numquam maxime exercitationem corrupti
+              voluptas. Distinctio, eveniet!
+            </p>
+          </div>
+        </div>
+
+        <div className="divider"></div>
+
+        <div className="container--large">
+          <div className="container--grey">
+            <p className="faq">
+              <span className="faq__qa">Question:</span> Grok?
+            </p>
+          </div>
+          <div className="container--white">
+            <p className="faq">
+              <span className="faq__qa">Answer:</span> Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Quis eum nulla enim aliquam!
+            </p>
+          </div>
+        </div>
+
+        <div className="divider"></div>
+      </section>
+
+      <Footer />
+    </>
+  );
+}
