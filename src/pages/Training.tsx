36b2108512a2c99b2bd04ad1d76275778625371d
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-09-22 10:17:55 +0100

    Refactor(training): replace Block with custom layout and Button component

--- src/pages/Training.tsx
+++ src/pages/Training.tsx
@@ -1,44 +1,49 @@
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navigation";
 import Footer from "../components/Footer";
-import Block from "../components/Block";
+import Button from "../components/Button";
 
 export default function Training() {
   return (
     <>
       <Navbar />
 
-      <section className="block block--grey block--top-castrated">
-        <h2>My Training</h2>
+      <section className="block block--white block--top-castrated">
+        <div className="container">
+          <h2 className="training__title">My Training</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+            deleniti vitae adipisci deserunt quibusdam nihil error excepturi
+            natus a dolorem.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+            delectus numquam cupiditate nam esse! Repellat, amet harum placeat
+            eius quae accusantium beatae dolore dolorum ad laudantium iure
+            itaque doloribus provident!
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+            ipsam? Tempore aut magni doloremque laboriosam eligendi pariatur
+            esse voluptatibus numquam? Enim nulla doloribus alias omnis odit
+            repellendus eum architecto animi reiciendis! At fugiat dolores ea
+            quibusdam eius minus blanditiis amet! Inventore, voluptatibus omnis
+            sequi id soluta incidunt harum repudiandae facilis!
+          </p>
+
+          <div className="grid grid--1x2 container__buttons">
+            <Link to="/services">
+              <Button>Services</Button>
+            </Link>
+            <Link to="/contact">
+              <Button>Contact</Button>
+            </Link>
+          </div>
+        </div>
       </section>
-
-      <Block
-        title=""
-        text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deleniti
-        vitae adipisci deserunt quibusdam nihil error excepturi natus a dolorem.
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-        delectus numquam cupiditate nam esse! Repellat, amet harum placeat eius
-        quae accusantium beatae dolore dolorum ad laudantium iure itaque
-        doloribus provident!
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-        ipsam? Tempore aut magni doloremque laboriosam eligendi pariatur esse
-        voluptatibus numquam? Enim nulla doloribus alias omnis odit repellendus
-        eum architecto animi reiciendis! At fugiat dolores ea quibusdam eius
-        minus blanditiis amet! Inventore, voluptatibus omnis sequi id soluta
-        incidunt harum repudiandae facilis!`}
-        buttonLabel="Services"
-        buttonHref="/services"
-        backgroundClass="block--grey"
-      />
-
-      <Block
-        title=""
-        text=""
-        buttonLabel="Contact"
-        buttonHref="/contact"
-        backgroundClass="block--grey"
-      />
 
       <section className="training__image"></section>
 
