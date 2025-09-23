95dcf46ef3f4129b958c285ab23c2d0d4364a4a9
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-09-23 18:59:56 +0100

    Add overlay for hero text on Home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,17 +5,19 @@ export default function Hero() {
   return (
     <section className="hero__container">
       <header className="block__header hero__nameplate">
-        <h1 className="hero__heading">Simon Burgess</h1>
-        <p className="hero__tagline">(MBACP)</p>
-        <p className="hero__tagline">Psychotherapist based in London, UK</p>
+        <div className="hero__text-overlay">
+          <h1 className="hero__heading">Simon Burgess</h1>
+          <p className="hero__tagline">(MBACP)</p>
+          <p className="hero__tagline">Psychotherapist based in London, UK</p>
 
-        <div className="grid grid--1x2 container__buttons">
-          <Link to="/about">
-            <Button>About</Button>
-          </Link>
-          <Link to="/contact">
-            <Button>Contact</Button>
-          </Link>
+          <div className="grid grid--1x2 container__buttons">
+            <Link to="/about">
+              <Button>About</Button>
+            </Link>
+            <Link to="/contact">
+              <Button>Contact</Button>
+            </Link>
+          </div>
         </div>
       </header>
     </section>
