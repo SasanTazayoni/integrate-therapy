25225fd95a10d1b4d3d2740cb9b8f7a6d2fddd05
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-09-25 16:14:31 +0100

    Extract hobbies data into separate file

---
+++ src/data/hobbies.ts
@@ -0,0 +1,7 @@
+export const hobbies = [
+  { src: "images/weightlifting.jpg", alt: "Weightlifting" },
+  { src: "images/tennis.jpg", alt: "Tennis racket" },
+  { src: "images/football.jpg", alt: "Football" },
+  { src: "images/reading.jpg", alt: "Reading a book" },
+  { src: "images/writing.jpg", alt: "Writing" },
+];
