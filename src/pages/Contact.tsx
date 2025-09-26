ebf4bb09b5280b8989877e64b0433e53fcbe8580
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-09-26 22:30:33 +0100

    Add data-testid to fees paragraph and title to map iframe

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -15,7 +15,7 @@ export default function Contact() {
 
           <div className="container--white contact__fees-card">
             <h3 className="block__header">Fees</h3>
-            <p>
+            <p data-testid="fees">
               <strong>Individuals:</strong> £85 per session (50 minutes)
             </p>
           </div>
@@ -43,6 +43,7 @@ export default function Contact() {
         <div className="container">
           <h3 className="block__header">Where to find us</h3>
           <iframe
+            title="map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2484.285644406752!2d-0.22501694831186675!3d51.48962547953231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760fb9f9740375%3A0x3da2c9c882211840!2s77%20Fulham%20Palace%20Rd%2C%20London%20W6%208JA!5e0!3m2!1sen!2suk!4v1637158751612!5m2!1sen!2suk"
             width="100%"
             height={600}
