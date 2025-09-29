98a87d429665fea7cefa7fe0fc823b0c75426929
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-09-29 21:37:35 +0100

    Change setupTests from js to ts file and update Vite config accordingly

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: "./setupTests.js",
+    setupFiles: "./setupTests.ts",
     coverage: {
       provider: "v8",
       reporter: ["text"],
