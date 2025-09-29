f91dda0cee7215c767a97f385e12dab08c639014
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-09-29 20:13:32 +0100

    Update NotFound page test suite

--- src/pages/NotFound.test.tsx
+++ src/pages/NotFound.test.tsx
@@ -1,21 +1,40 @@
 import { describe, test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import NotFound from "./NotFound";
 
 describe("NotFound Page", () => {
-  test("renders Navbar and Footer", () => {
+  test("renders Navbar correctly", () => {
     render(
       <MemoryRouter>
         <NotFound />
       </MemoryRouter>
     );
 
-    const navbar = screen.getByRole("navigation");
+    const navbar = screen.getByTestId("navbar");
     expect(navbar).toBeInTheDocument();
 
-    const footer = screen.getByTestId("footer-text");
-    expect(footer).toBeInTheDocument();
+    const navLinks = within(navbar).getAllByRole("link", { name: /page/i });
+    expect(navLinks.length).toBe(5);
+  });
+
+  test("renders Footer correctly", () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>
+    );
+
+    const footerNav = screen.getByLabelText("Footer Navigation");
+    expect(footerNav).toBeInTheDocument();
+
+    const footerLinks = within(footerNav).getAllByRole("link");
+    expect(footerLinks.length).toBe(5);
+    expect(footerLinks[0]).toHaveAttribute("href", "/");
+    expect(footerLinks[1]).toHaveAttribute("href", "/about");
+    expect(footerLinks[2]).toHaveAttribute("href", "/services");
+    expect(footerLinks[3]).toHaveAttribute("href", "/faq");
+    expect(footerLinks[4]).toHaveAttribute("href", "/contact");
   });
 
   test("renders main heading", () => {
@@ -43,7 +62,7 @@ describe("NotFound Page", () => {
     );
     expect(paragraph).toBeInTheDocument();
 
-    const homeLink = screen.getByRole("link", { name: /home page/i });
+    const homeLink = screen.getByTestId("notfound-home-link");
     expect(homeLink).toBeInTheDocument();
     expect(homeLink).toHaveAttribute("href", "/");
   });
