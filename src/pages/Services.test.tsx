54e5cce38ee97480f3fe349e357c251d4dce844b
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-09-30 10:31:26 +0100

    Update Services page test suite

--- src/pages/Services.test.tsx
+++ src/pages/Services.test.tsx
@@ -4,18 +4,41 @@ import { MemoryRouter } from "react-router-dom";
 import Services from "./Services";
 
 describe("Services Page", () => {
-  test("renders Navbar and Footer", () => {
+  test("renders Navbar correctly", () => {
     render(
       <MemoryRouter>
         <Services />
       </MemoryRouter>
     );
 
-    const navbar = screen.getByRole("navigation");
+    const navbar = screen.getByTestId("navbar");
     expect(navbar).toBeInTheDocument();
+
+    const navLinks = within(navbar).getAllByRole("link");
+    expect(navLinks.length).toBeGreaterThan(0);
+  });
+
+  test("renders Footer correctly", () => {
+    render(
+      <MemoryRouter>
+        <Services />
+      </MemoryRouter>
+    );
 
     const footer = screen.getByTestId("footer-text");
     expect(footer).toBeInTheDocument();
+
+    const footerNav = screen.getByLabelText("Footer Navigation");
+    expect(footerNav).toBeInTheDocument();
+
+    const footerLinks = within(footerNav).getAllByRole("link");
+    expect(footerLinks.length).toBe(5);
+
+    expect(footerLinks[0]).toHaveAttribute("href", "/");
+    expect(footerLinks[1]).toHaveAttribute("href", "/about");
+    expect(footerLinks[2]).toHaveAttribute("href", "/services");
+    expect(footerLinks[3]).toHaveAttribute("href", "/faq");
+    expect(footerLinks[4]).toHaveAttribute("href", "/contact");
   });
 
   test("renders all service list items", () => {
@@ -29,8 +52,10 @@ describe("Services Page", () => {
     const serviceListItems = within(servicesList).getAllByRole("listitem");
     expect(serviceListItems.length).toBe(30);
 
-    expect(serviceListItems[0].textContent).toBe("Anxiety and Panic Disorders");
-    expect(serviceListItems[29].textContent).toBe(
+    expect(serviceListItems[0]).toHaveTextContent(
+      "Anxiety and Panic Disorders"
+    );
+    expect(serviceListItems[29]).toHaveTextContent(
       "Support for Carers and Helping Professionals"
     );
   });
